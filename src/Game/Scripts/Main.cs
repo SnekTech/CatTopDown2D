f3af40c1d503d00c5f4ab46d703eb7d454b8ae96
syf <2654525303@qq.com>
2025-04-25 16:40:01 +0800

    set up map scene using TileMapLayer

--- src/Game/Scripts/Main.cs
+++ src/Game/Scripts/Main.cs
@@ -2,10 +2,6 @@
 
 namespace CatTopDown2D;
 
-public partial class Main: Node2D
+public partial class Main: Node
 {
-    public override void _Ready()
-    {
-        GD.Print("Hello from pixel art template!");
-    }
 }
