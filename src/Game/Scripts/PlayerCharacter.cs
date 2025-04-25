25ad7414546fa3c1459bb989f19c0381de6bfccb
syf <2654525303@qq.com>
2025-04-25 21:14:38 +0800

    player movement

---
+++ src/Game/Scripts/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+namespace CatTopDown2D;
+
+public partial class PlayerCharacter : CharacterBody2D
+{
+    [Export(PropertyHint.Range, "0,1000")] public int Speed { get; private set; } = 60;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GetPlayerInput();
+        MoveAndSlide();
+    }
+
+    private void GetPlayerInput()
+    {
+        var vector = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Velocity = vector * Speed;
+    }
+}
