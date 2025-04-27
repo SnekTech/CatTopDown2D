1cbdc000319e287470aa8a4910c909c728773a6b
syf <2654525303@qq.com>
2025-04-27 21:46:34 +0800

    detector pulsing invalid color

--- src/Game/Scripts/Detector.cs
+++ src/Game/Scripts/Detector.cs
@@ -12,10 +12,14 @@ public partial class Detector : Area2D
     [Export(PropertyHint.ColorNoAlpha)]
     private Color validColor = Colors.Green;
 
+    [Export(PropertyHint.Range, "0.1,1")]
+    private float pulseFrequency = 0.25f;
+
     [Node]
     private Sprite2D sprite2D = null!;
 
     private int objectCount;
+    private float pulseProgress;
 
     public override void _Notification(int what)
     {
@@ -29,6 +33,28 @@ public partial class Detector : Area2D
     {
         BodyEntered += OnBodyEntered;
         BodyExited += OnBodyExited;
+
+        pulseProgress = GD.Randf();
+    }
+
+    public override void _Process(double delta)
+    {
+        if (objectCount == 0)
+        {
+            pulseProgress += (float)delta * pulseFrequency;
+            if (pulseProgress >= 1)
+            {
+                pulseProgress -= 1;
+            }
+
+            var brightness = Mathf.Cos(pulseProgress * float.Tau) * 0.25f + 0.75f;
+            sprite2D.Modulate = invalidColor with
+            {
+                R = invalidColor.R * brightness,
+                G = invalidColor.G * brightness,
+                B = invalidColor.B * brightness
+            };
+        }
     }
 
     private void OnBodyEntered(Node2D node)
@@ -46,7 +72,7 @@ public partial class Detector : Area2D
         objectCount--;
         if (objectCount == 0)
         {
-            sprite2D.Modulate = invalidColor;
+            pulseProgress = 0;
         }
     }
 }
